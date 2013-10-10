9a177ca556401716273b1ee6f67883677f832aaa
Geoffrey <user@ubuntu.(none)>
2013-10-10 09:27:00 +0200

    debug

--- class/actions_multidevise.class.php
+++ class/actions_multidevise.class.php
@@ -271,7 +271,7 @@ class ActionsMultidevise
 							$res = $db->fetch_object($resql);
 							
 							if($line->rowid == $_REQUEST['lineid']){
-								echo "$('#product_desc').parent().next().next().after('<td align=\"right\"><input type=\"text\" value=\"".price($res->devise_pu,0,'',1,2,2)."\" name=\"dp_pu_devise\" size=\"6\"></td>');";
+								echo "$('#product_desc').parent().next().next().after('<td align=\"right\"><input type=\"text\" value=\"".price2num($res->devise_pu,2)."\" name=\"dp_pu_devise\" size=\"6\"></td>');";
 								echo "$('input[name=pu_devise]').val(".price($res->devise_pu,0,'',1,2,2).");";
 								echo "$('#product_desc').parent().next().next().next().next().next().after('<td align=\"right\"></td>');";
 							}
