516e6686f21c9b8e0610685c9d28498e944ebb0e
geoffrey.girard <geoffre.girard@atm-consulting.fr>
2014-04-11 15:41:44 +0200

    ajout du test si product_multiprice est actif

--- js/hook.js.php
+++ js/hook.js.php
@@ -5,88 +5,94 @@
 	/*
 	 * Ajoute le choix de currency 
 	 */
-	$form=new Form($db);
-	ob_start();
-	$form->select_currency($cur,"currency");
-	$select_currency =ob_get_clean();
-
-	$res =$db->query("SELECT DISTINCT devise_code, price_level FROM ".MAIN_DB_PREFIX."product_price ORDER BY date_price DESC");
-	$Tab=array();
-	while($obj = $db->fetch_object($res)) {
-		if($obj->price_level>1) {
-			$Tab[$obj->price_level] = $obj->devise_code;	
-		}
-				
-	}
+	if($conf->global->PRODUCT_MULTIPRICE){
 	
-	$referrer = $_SERVER['HTTP_REFERER'];	
-
-?>
-
-
-$(document).ready(function() {
-	var ref="<?=$referrer?>";
-	if(ref.indexOf('/htdocs/product/price.php?action=edit_price')!=-1) {
-		/*
-		 * Fiche price
-		 */
+		$form=new Form($db);
+		ob_start();
+		$form->select_currency($cur,"currency");
+		$select_currency =ob_get_clean();
 		
-		var i=1;
-		
-		$("div.fiche [name^=multiprices_base_type_]").each(function() {
-			
-			if(i==1) {
-				null; // monnaie courante du logiciel obligatoire
-			}
-			else {
-				$select = $("<?=addslashes($select_currency ) ?>");
-				$select.attr('id', 'currency_'+i);
-				
-				$(this).after($select);	
+		$res =$db->query("SELECT DISTINCT devise_code, price_level FROM ".MAIN_DB_PREFIX."product_price ORDER BY date_price DESC");
+		$Tab=array();
+		while($obj = $db->fetch_object($res)) {
+			if($obj->price_level>1) {
+				$Tab[$obj->price_level] = $obj->devise_code;	
 			}
 					
-			i++;
-		});
-		
-		<?php
-		
-		foreach($Tab as $price_level=>$cur) {
-			?>
-			$('#currency_<?=$price_level ?>').val('<?=$cur ?>');
-			<?
 		}
 		
-		
-		?>
-		
-	}
-	else if(ref.indexOf('/htdocs/product/price.php?id=')!=-1) {
-		var TCurrency = new Array;
-		<?php
-		
-		foreach($Tab as $price_level=>$cur) {
-			?>
-			TCurrency[<?=$price_level ?>] = '<?=$cur ?>';
-			<?
-		}
-		?>
-		
-		$('div.fiche table tr>td+td').each(function() {
+		$referrer = $_SERVER['HTTP_REFERER'];
+
+	?>
+	
+	
+	$(document).ready(function() {
+		var ref="<?=$referrer?>";
+		if(ref.indexOf('/htdocs/product/price.php?action=edit_price')!=-1) {
+			/*
+			 * Fiche price
+			 */
 			
-			price_level = $(this).html();
+			var i=1;
 			
-			if(TCurrency[price_level]!=null) {
-				$(this).next().next().next().append(' '+TCurrency[price_level]);
+			$("div.fiche [name^=multiprices_base_type_]").each(function() {
+				
+				if(i==1) {
+					null; // monnaie courante du logiciel obligatoire
+				}
+				else {
+					$select = $("<?=addslashes($select_currency ) ?>");
+					$select.attr('id', 'currency_'+i);
+					
+					$(this).after($select);	
+				}
+						
+				i++;
+			});
+			
+			<?php
+			
+			foreach($Tab as $price_level=>$cur) {
+				?>
+				$('#currency_<?=$price_level ?>').val('<?=$cur ?>');
+				<?
 			}
 			
-		});
+			
+			?>
+			
+		}
+		else if(ref.indexOf('/htdocs/product/price.php?id=')!=-1) {
+			var TCurrency = new Array;
+			<?php
+			
+			foreach($Tab as $price_level=>$cur) {
+				?>
+				TCurrency[<?=$price_level ?>] = '<?=$cur ?>';
+				<?
+			}
+			?>
+			
+			$('div.fiche table tr>td+td').each(function() {
+				
+				price_level = $(this).html();
+				
+				if(TCurrency[price_level]!=null) {
+					$(this).next().next().next().append(' '+TCurrency[price_level]);
+				}
+				
+			});
+			
+		}
+		else {
+			/*alert(document.referrer);*/
+		}
 		
+		
+		
+	});
+	
+	<?php
 	}
-	else {
-		/*alert(document.referrer);*/
-	}
-	
-	
-	
-});
+?>
 	
