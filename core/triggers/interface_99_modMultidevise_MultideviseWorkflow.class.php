2be772d4dcd3e2c8c41d96f1d2033e23a9418d5c
geoffrey.girard <geoffre.girard@atm-consulting.fr>
2014-04-25 17:27:46 +0200

    debug trigger insertion ligne facture fournisseur

--- core/triggers/interface_99_modMultidevise_MultideviseWorkflow.class.php
+++ core/triggers/interface_99_modMultidevise_MultideviseWorkflow.class.php
@@ -356,11 +356,14 @@ class InterfaceMultideviseWorkflow
 					}
 					
 					/*echo '<pre>';
-					print_r($object);
-					echo '</pre>';exit;*/
+					print_r($_REQUEST);
+					echo '</pre>'; exit;*/
 					
 					if(get_class($object)=='CommandeFournisseur') {
 						$object->updateline($object->rowid, $ligne->desc, $subprice, $ligne->qty, $ligne->remise_percent, $ligne->tva_tx);
+					}
+					elseif(defined('BUY_PRICE_IN_CURRENCY') && BUY_PRICE_IN_CURRENCY && $action == 'LINEBILL_SUPPLIER_CREATE'){
+						$object->updateline($object->rowid, $ligne->description, $object->subprice, $ligne->tva_tx,0,0,$_REQUEST['qty'],$ligne->product_id);
 					}
 					else {
 						$object->update(1);
