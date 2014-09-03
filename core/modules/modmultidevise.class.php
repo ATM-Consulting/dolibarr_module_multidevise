feff3ae044bd8f8324dcb65c489760bfce00b121
maxime peyrot <maxime.peyrot@atm-consulting.fr>
2014-09-03 16:32:05 +0200

    Création du nouveau modèle de PDF multidevises

--- core/modules/modmultidevise.class.php
+++ core/modules/modmultidevise.class.php
@@ -73,7 +73,6 @@ class modmultidevise extends DolibarrModules
 		// for specific path of parts (eg: /mymodule/core/modules/barcode)
 		// for specific css file (eg: /mymodule/css/mymodule.css.php)
 		//$this->module_parts = array('triggers' => 1);
-		//$this->module_parts = array(
 		//                        	'triggers' => 1                                 	// Set this to 1 if module has its own trigger directory (core/triggers)
 		//							'login' => 0,                                    	// Set this to 1 if module has its own login method directory (core/login)
 		//							'substitutions' => 0,                            	// Set this to 1 if module has its own substitution function file (core/substitutions)
@@ -81,13 +80,14 @@ class modmultidevise extends DolibarrModules
 		//							'theme' => 0,                                    	// Set this to 1 if module has its own theme directory (core/theme)
 		//                        	'tpl' => 0,                                      	// Set this to 1 if module overwrite template dir (core/tpl)
 		//							'barcode' => 0,                                  	// Set this to 1 if module has its own barcode directory (core/modules/barcode)
-		//							'models' => 0,                                   	// Set this to 1 if module has its own models directory (core/modules/xxx)
+		//	'models' => 1,                                   	// Set this to 1 if module has its own models directory (core/modules/xxx)
 		//							'css' => array('/mymodule/css/mymodule.css.php'),	// Set this to relative path of css file if module has its own css file
 	 	//							'js' => array('/mymodule/js/mymodule.js'),          // Set this to relative path of js file if module must load a js on all pages
 		//							'hooks' => array('hookcontext1','hookcontext2')  	// Set here all hooks context managed by module
 		//							'dir' => array('output' => 'othermodulename'),      // To force the default directories names
 		//							'workflow' => array('WORKFLOW_MODULE1_YOURACTIONTYPE_MODULE2'=>array('enabled'=>'! empty($conf->module1->enabled) && ! empty($conf->module2->enabled)', 'picto'=>'yourpicto@mymodule')) // Set here all workflow context managed by module
-		//	                       );
+		//);
+		
 		$this->module_parts = array(
 			'hooks'=>array(
 				'thirdpartycard',
@@ -102,7 +102,9 @@ class modmultidevise extends DolibarrModules
 				'pricesuppliercard'
 			)
 			,'triggers'=>1
-			,'js'=>'/multidevise/js/hook.js.php');
+			,'js'=>'/multidevise/js/hook.js.php'
+			,'models' => 1
+		);
 
 		// Data directories to create when module is enabled.
 		// Example: this->dirs = array("/mymodule/temp");
