5cf9a97f93732d4a6a846c3bf92d900d7e00e586
maxime peyrot <maxime.peyrot@atm-consulting.fr>
2015-07-08 09:32:22 +0200

    FIX : Intégration de méthode intermédiaire pour récupérer les fichiers depuis notre serveur (devises et taux)

--- cron/1day/sync.php
+++ cron/1day/sync.php
@@ -18,7 +18,7 @@
 	$url_list = TCurrenty_list_source;
 	$url_rate =  strtr(TCurrenty_rate_source,array('{app_id}'=>TCurrenty_app_id));
 	
-	$TCurrency = json_decode( file_get_contents($url_list) );
+	$TCurrency = url_get_content($url_list);
 	
 	$db=new TPDOdb;	
 		
@@ -35,7 +35,7 @@
 		
 	}
 
-	$TRate = json_decode( file_get_contents($url_rate) );
+	$TRate = url_get_content($url_rate);
 	
 	//Récupération des devises à convertir
 	$TFromTo = explode(',',TCurrenty_from_to_rate);
@@ -90,4 +90,78 @@
 				}
 			}
 		}
-	}	
+	}
+
+function url_get_content($url) {
+	$TResults = array();
+	
+	if (ini_get('allow_url_fopen')) {
+		$TResults = json_decode(file_get_contents($url));
+	} else {
+		if (function_exists('curl_init')) {
+			$TResults = json_decode(load_curl($url));
+		} else {
+			$TResults = json_decode(fetchURL($url));
+		}
+	}
+	
+	return $TResults;
+}
+
+// Fonction appelée si allow_url_fopen = Off
+function fetchURL($url){
+	$url_parsed = parse_url($url);
+	
+	$host = $url_parsed['host'];
+	$port = $url_parsed['port'];
+	$path = $url_parsed['path'];
+	
+	if (!$port) {
+		$port = 80;
+	}
+	
+	if ($url_parsed['query'] != '') {
+		$path.="?".$url_parsed['query'];
+	}
+	
+	$out = "GET $path HTTP/1.0\r\nHost: $host\r\n\r\n";
+	$fp  = @fsockopen($host, $port, $errno, $errstr, 30);
+	
+	if (!$fp) {
+		return 0;
+		exit;
+	}
+	
+	fwrite($fp, $out);
+	$in = '';
+	$header = '';
+	
+	// Passer l'en-tête HTML
+	do {
+		$header .= fgets ($fp, 4096 );
+	} while (strpos($header, "\r\n\r\n" ) === false);
+	
+	// Lecture du contenu du fichier
+	while (!feof($fp)) {
+		$s = fgets($fp, 4096);
+		$in.=$s;
+	}
+	
+	fclose($fp);
+	return $in;
+}
+
+function load_curl($url) {
+	// Initialisation
+	$curl = curl_init($url);
+	
+	// Option permettant de récupérer directement sous forme de chaine la valeur retournée par curl_exec()
+	curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+	
+	$result = curl_exec($curl);
+	
+	curl_close($curl);
+	
+	return $result;
+}
+
