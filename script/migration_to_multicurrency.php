f6eeac382fd26d1cebed260b527f30f0e13cd9e4
atm-ph <phf@atm-consulting.Fr>
2017-09-15 16:00:50 +0200

    Fix manque de controle sur la gestion d'erreur

--- script/migration_to_multicurrency.php
+++ script/migration_to_multicurrency.php
@@ -83,18 +83,32 @@ if (!$error)
 }
 
 // TODO update llx_societe @see TMultideviseClient
+echo 'Update '.MAIN_DB_PREFIX.'societe ...';
 $sql = 'UPDATE '.MAIN_DB_PREFIX.'societe s
 		INNER JOIN '.MAIN_DB_PREFIX.'multicurrency m ON (m.code = s.devise_code AND m.entity = s.entity)
 		SET s.multicurrency_code=s.devise_code, s.fk_multicurrency=m.rowid';
 $resql = $db->query($sql);
+if (!$resql)
+{
+	$error++;
+	$TError[] = $db->lasterror();
+}
 
 // TODO update llx_product_price @see TMultideviseProductPrice
+echo 'Update '.MAIN_DB_PREFIX.'product_price step 1...';
 // 1er update pour avoir le bon code devise + fk_multicurrency
 $sql = 'UPDATE '.MAIN_DB_PREFIX.'product_price pp
 		INNER JOIN '.MAIN_DB_PREFIX.'multicurrency m ON (m.code = pp.devise_code AND m.entity = pp.entity)
 		SET	 pp.multicurrency_code = pp.devise_code
 			,pp.fk_multicurrency = m.rowid';
 $resql = $db->query($sql);
+if (!$resql)
+{
+	$error++;
+	$TError[] = $db->lasterror();
+}
+
+echo 'Update '.MAIN_DB_PREFIX.'product_price step 2...';
 // 2eme update pour s'occuper du prix HT, taux et prix TTC
 $sql = 'UPDATE '.MAIN_DB_PREFIX.'product_price pp,
 		(
@@ -111,10 +125,15 @@ $sql = 'UPDATE '.MAIN_DB_PREFIX.'product_price pp,
 			,pp.multicurrency_tx = t.rate
 			,pp.multicurrency_price_ttc = pp.price_ttc * t.rate';
 $resql = $db->query($sql);
-
+if (!$resql)
+{
+	$error++;
+	$TError[] = $db->lasterror();
+}
 
 
 // TODO update llx_propal @see TMultidevisePropal
+echo 'Update '.MAIN_DB_PREFIX.'propal...';
 $sql = 'UPDATE '.MAIN_DB_PREFIX.'propal p
 		INNER JOIN '.MAIN_DB_PREFIX.'multicurrency m ON (m.code = p.devise_code AND m.entity = p.entity)
 		SET p.multicurrency_code = p.devise_code
@@ -124,8 +143,14 @@ $sql = 'UPDATE '.MAIN_DB_PREFIX.'propal p
 			,p.multicurrency_total_tva = p.tva * p.devise_taux
 			,p.multicurrency_total_ttc = p.total * p.devise_taux';
 $resql = $db->query($sql);
+if (!$resql)
+{
+	$error++;
+	$TError[] = $db->lasterror();
+}
 
 // TODO update llx_propaldet @see TMultidevisePropaldet
+echo 'Update '.MAIN_DB_PREFIX.'propaldet...';
 $sql = 'UPDATE '.MAIN_DB_PREFIX.'propaldet pd
 		INNER JOIN '.MAIN_DB_PREFIX.'propal p ON (p.rowid = pd.fk_propal)
 		SET pd.fk_multicurrency = p.fk_multicurrency
@@ -135,10 +160,15 @@ $sql = 'UPDATE '.MAIN_DB_PREFIX.'propaldet pd
 			,pd.multicurrency_total_tva = pd.total_tva * p.multicurrency_tx
 			,pd.multicurrency_total_ttc = pd.total_ttc * p.multicurrency_tx';
 $resql = $db->query($sql);
-
+if (!$resql)
+{
+	$error++;
+	$TError[] = $db->lasterror();
+}
 
 
 // TODO update llx_commande @see TMultideviseCommande
+echo 'Update '.MAIN_DB_PREFIX.'commande...';
 $sql = 'UPDATE '.MAIN_DB_PREFIX.'commande c
 		INNER JOIN '.MAIN_DB_PREFIX.'multicurrency m ON (m.code = c.devise_code AND m.entity = c.entity)
 		SET c.multicurrency_code = c.devise_code
@@ -148,8 +178,14 @@ $sql = 'UPDATE '.MAIN_DB_PREFIX.'commande c
 			,c.multicurrency_total_tva = c.tva * c.devise_taux
 			,c.multicurrency_total_ttc = c.total_ttc * c.devise_taux';
 $resql = $db->query($sql);
+if (!$resql)
+{
+	$error++;
+	$TError[] = $db->lasterror();
+}
 
 // TODO update llx_commandedet @see TMultideviseCommandedet
+echo 'Update '.MAIN_DB_PREFIX.'commandedet...';
 $sql = 'UPDATE '.MAIN_DB_PREFIX.'commandedet cd
 		INNER JOIN '.MAIN_DB_PREFIX.'commande c ON (c.rowid = cd.fk_commande)
 		SET cd.fk_multicurrency = c.fk_multicurrency
@@ -159,10 +195,15 @@ $sql = 'UPDATE '.MAIN_DB_PREFIX.'commandedet cd
 			,cd.multicurrency_total_tva = cd.total_tva * c.multicurrency_tx
 			,cd.multicurrency_total_ttc = cd.total_ttc * c.multicurrency_tx';
 $resql = $db->query($sql);
-
+if (!$resql)
+{
+	$error++;
+	$TError[] = $db->lasterror();
+}
 
 
 // TODO update llx_facture @see TMultideviseFacture
+echo 'Update '.MAIN_DB_PREFIX.'facture ...';
 $sql = 'UPDATE '.MAIN_DB_PREFIX.'facture f
 		INNER JOIN '.MAIN_DB_PREFIX.'multicurrency m ON (m.code = f.devise_code AND m.entity = f.entity)
 		SET f.multicurrency_code = f.devise_code
@@ -172,8 +213,15 @@ $sql = 'UPDATE '.MAIN_DB_PREFIX.'facture f
 			,f.multicurrency_total_tva = f.tva * f.devise_taux
 			,f.multicurrency_total_ttc = f.total_ttc * f.devise_taux';
 $resql = $db->query($sql);
+if (!$resql)
+{
+	echo 'ERROR<br />';
+	$error++;
+	$TError[] = $db->lasterror();
+} else echo 'OK<br />';
 
 // TODO update llx_facturedet @see TMultideviseFacturedet
+echo 'Update '.MAIN_DB_PREFIX.'facturedet...';
 $sql = 'UPDATE '.MAIN_DB_PREFIX.'facturedet fd
 		INNER JOIN '.MAIN_DB_PREFIX.'facture f ON (f.rowid = fd.fk_facture)
 		SET fd.fk_multicurrency = f.fk_multicurrency
@@ -183,10 +231,16 @@ $sql = 'UPDATE '.MAIN_DB_PREFIX.'facturedet fd
 			,fd.multicurrency_total_tva = fd.total_tva * f.multicurrency_tx
 			,fd.multicurrency_total_ttc = fd.total_ttc * f.multicurrency_tx';
 $resql = $db->query($sql);
-
+if (!$resql)
+{
+	echo 'ERROR<br />';
+	$error++;
+	$TError[] = $db->lasterror();
+} else echo 'OK<br />';
 
 
 // TODO update llx_commande_fournisseur @see TMultideviseCommandeFournisseur
+echo 'Update '.MAIN_DB_PREFIX.'commande_fournisseur...';
 $sql = 'UPDATE '.MAIN_DB_PREFIX.'commande_fournisseur cf
 		INNER JOIN '.MAIN_DB_PREFIX.'multicurrency m ON (m.code = cf.devise_code AND m.entity = cf.entity)
 		SET cf.multicurrency_code = cf.devise_code
@@ -196,8 +250,15 @@ $sql = 'UPDATE '.MAIN_DB_PREFIX.'commande_fournisseur cf
 			,cf.multicurrency_total_tva = cf.tva * cf.devise_taux
 			,cf.multicurrency_total_ttc = cf.total_ttc * cf.devise_taux';
 $resql = $db->query($sql);
+if (!$resql)
+{
+	echo 'ERROR<br />';
+	$error++;
+	$TError[] = $db->lasterror();
+} else echo 'OK<br />';
 
 // TODO update llx_commande_fournisseurdet @see TMultideviseCommandeFournisseurdet
+echo 'Update '.MAIN_DB_PREFIX.'commande_fournisseurdet...';
 $sql = 'UPDATE '.MAIN_DB_PREFIX.'commande_fournisseurdet cfd
 		INNER JOIN '.MAIN_DB_PREFIX.'commande_fournisseur cf ON (cf.rowid = cfd.fk_commande)
 		SET cfd.fk_multicurrency = cf.fk_multicurrency
@@ -207,10 +268,16 @@ $sql = 'UPDATE '.MAIN_DB_PREFIX.'commande_fournisseurdet cfd
 			,cfd.multicurrency_total_tva = cfd.total_tva * cf.multicurrency_tx
 			,cfd.multicurrency_total_ttc = cfd.total_ttc * cf.multicurrency_tx';
 $resql = $db->query($sql);
-
+if (!$resql)
+{
+	echo 'ERROR<br />';
+	$error++;
+	$TError[] = $db->lasterror();
+} else echo 'OK<br />';
 
 
 // TODO update llx_facture_fourn @see TMultideviseFactureFournisseur
+echo 'Update '.MAIN_DB_PREFIX.'facture_fourn...';
 $sql = 'UPDATE '.MAIN_DB_PREFIX.'facture_fourn ff
 		INNER JOIN '.MAIN_DB_PREFIX.'multicurrency m ON (m.code = ff.devise_code AND m.entity = ff.entity)
 		SET ff.multicurrency_code = ff.devise_code
@@ -220,8 +287,15 @@ $sql = 'UPDATE '.MAIN_DB_PREFIX.'facture_fourn ff
 			,ff.multicurrency_total_tva = ff.total_tva * ff.devise_taux
 			,ff.multicurrency_total_ttc = ff.total_ttc * ff.devise_taux';
 $resql = $db->query($sql);
+if (!$resql)
+{
+	echo 'ERROR<br />';
+	$error++;
+	$TError[] = $db->lasterror();
+} else echo 'OK<br />';
 
 // TODO update llx_facture_fourn_det @see TMultideviseFactureFournisseurdet
+echo 'Update '.MAIN_DB_PREFIX.'facture_fourn_det...';
 $sql = 'UPDATE '.MAIN_DB_PREFIX.'facture_fourn_det ffd
 		INNER JOIN '.MAIN_DB_PREFIX.'facture_fourn ff ON (ff.rowid = ffd.fk_facture_fourn)
 		SET ffd.fk_multicurrency = ff.fk_multicurrency
@@ -231,18 +305,36 @@ $sql = 'UPDATE '.MAIN_DB_PREFIX.'facture_fourn_det ffd
 			,ffd.multicurrency_total_tva = ffd.total_tva * ff.multicurrency_tx
 			,ffd.multicurrency_total_ttc = ffd.total_ttc * ff.multicurrency_tx';
 $resql = $db->query($sql);
-
+if (!$resql)
+{
+	echo 'ERROR<br />';
+	$error++;
+	$TError[] = $db->lasterror();
+} else echo 'OK<br />';
 
 
 // TODO update llx_paiement_facture @see TMultidevisePaiementFacture
+echo 'Update '.MAIN_DB_PREFIX.'paiement_facture...';
 $sql = 'UPDATE '.MAIN_DB_PREFIX.'paiement_facture
 		SET multicurrency_amount = amount * devise_taux';
-
+if (!$resql)
+{
+	echo 'ERROR<br />';
+	$error++;
+	$TError[] = $db->lasterror();
+} else echo 'OK<br />';
 
 
 // TODO update llx_paiementfourn_facturefourn @see TMultidevisePaiementFactureFournisseur
+echo 'Update '.MAIN_DB_PREFIX.'paiementfourn_facturefourn...';
 $sql = 'UPDATE '.MAIN_DB_PREFIX.'paiementfourn_facturefourn
 		SET multicurrency_amount = amount * devise_taux';
+if (!$resql)
+{
+	echo 'ERROR<br />';
+	$error++;
+	$TError[] = $db->lasterror();
+} else echo 'OK<br />';
 
 
 if (!$error)
